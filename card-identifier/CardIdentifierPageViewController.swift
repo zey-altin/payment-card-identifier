aeaab5b6a907b9e775927a92a906ec89ac8d1772
Zeynep Nur Altın <zeynepnuraltin@gmail.com>
2024-08-14 00:04:21 +0300

    added image picker

--- card-identifier/CardIdentifierPageViewController.swift
+++ card-identifier/CardIdentifierPageViewController.swift
@@ -27,32 +27,45 @@ class CardIdentifierPageViewController: UIViewController {
     
     private let imageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "example1")
+        imageView.image = UIImage(systemName: "square.and.arrow.down")
         imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
         return imageView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(cardNumberLabel)
         view.addSubview(imageView)
+        view.addSubview(cardNumberLabel)
         view.addSubview(cardOwnerNameLabel)
         view.addSubview(cardExpiryDateLabel)
         
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .white
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        imageView.addGestureRecognizer(tapGesture)
         setupLabels()
-        
         recognizeText(image: imageView.image)
     }
+        
+    // MARK: - Selectors
+    @objc private func imageTapped() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true)
+    }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.frame = CGRect(
             x: 20,
-            y: view.safeAreaInsets.top,
+            y: view.safeAreaInsets.top+100,
             width: view.frame.size.width-40,
-            height: view.frame.size.width-40)
+            height: 200)
         cardNumberLabel.frame = CGRect(
             x: 20,
             y: imageView.frame.maxY + 40,
@@ -84,10 +97,24 @@ class CardIdentifierPageViewController: UIViewController {
         label.text = text
     }
     
+    func clearLabels() {
+        cardNumberLabel.text = "Kart numarası: "
+        cardOwnerNameLabel.text = "Kart sahibi: "
+        cardExpiryDateLabel.text = "SKT: "
+    }
+    
+    func resetCardDetails() {
+        cardDetails = CardDetails() // Reset the card details
+        clearLabels() // Clear the labels
+    }
+    
     func recognizeText(image: UIImage?){
+        resetCardDetails()
+        
         guard let cgImage = image?.cgImage else {
             fatalError("Error: Could not get cgImage.")
         }
+        print("Processing image for text recognition...")
         
         //Handler
         let handler = VNImageRequestHandler(cgImage: cgImage, options: [ : ])
@@ -102,9 +129,9 @@ class CardIdentifierPageViewController: UIViewController {
                 guard let recognizedText = observation.topCandidates(1).first?.string else { continue }
                 print("Recognized text: \(recognizedText)")
                 if recognizedText.range(of: #"^\d{4} \d{4} \d{4} \d{4}$"#, options: .regularExpression) != nil, self?.cardDetails.cardNumber == nil {
-                    self?.cardNumberLabel.text? += "\(recognizedText)"
-                    self?.cardDetails.cardNumber = recognizedText
-                    print("**Kart numarası: \(self?.cardDetails.cardNumber ?? "")")
+                        self?.cardNumberLabel.text? += "\(recognizedText)"
+                        self?.cardDetails.cardNumber = recognizedText
+                        print("**Kart numarası: \(self?.cardDetails.cardNumber ?? "")")
                 }
                 else if self?.cardDetails.name == nil,
                         !(self?.ignoreList.contains(where: recognizedText.contains))!,
@@ -116,12 +143,11 @@ class CardIdentifierPageViewController: UIViewController {
                 
                 else if self?.cardDetails.expiryDate == nil,
                     recognizedText.range(of: #"([A-Z]+:\s)*(0[0-9]|1[0-2])\/([0-4][0-9])"#, options: .regularExpression) != nil {
-                    let cleanedText = recognizedText.replacingOccurrences(of:  #"[^0-9/]"#, with: "", options: .regularExpression)
+                        let cleanedText = recognizedText.replacingOccurrences(of:  #"[^0-9/]"#, with: "", options: .regularExpression)
                         self?.cardExpiryDateLabel.text? += "\(cleanedText)"
                         self?.cardDetails.expiryDate = cleanedText
                         print("**Kart SKT: \(self?.cardDetails.expiryDate ?? "")")
                 }
-                
             }
         }
         
@@ -137,3 +163,20 @@ class CardIdentifierPageViewController: UIViewController {
     }
 }
 
+extension CardIdentifierPageViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+                
+        if let editedImage = info[.editedImage] as? UIImage {
+            imageView.image = editedImage
+            recognizeText(image: editedImage)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            imageView.image = originalImage
+            recognizeText(image: originalImage)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
